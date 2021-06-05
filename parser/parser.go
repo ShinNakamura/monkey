f956069a962354cd1181ae6cf04f7e9179149dc8
Shin Nakamura <kokoronopython@gmail.com>
2021-06-05 12:53:18 +0900

    2.6.5 P.55

--- parser/parser.go
+++ parser/parser.go
@@ -8,13 +8,20 @@ import (
 )
 
 type Parser struct {
-	l *lexer.Lexer
+	l      *lexer.Lexer
+	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
 
-	errors []string
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
+
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
 
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{
